0de18ea08c93912137f1734abd6b584410a896ca
Luca-Paul Schirmer <lucapaulschirmer@gmail.com>
2026-05-04 21:31:21 +0200

    Changed the output structure of XOR Split Gateways from gateways and tasks to gateways, flows and tasks

--- src/bpmn_model_analyzer/bpmn_data_extractor.py
+++ src/bpmn_model_analyzer/bpmn_data_extractor.py
@@ -37,15 +37,20 @@ def get_first_target_tasks_from_gateway(flow: pm4py.BPMN.Flow, visited = None) -
 
 def get_xor_split_gateway_target_tasks(bpmn_model: pm4py.BPMN) -> dict:
     """
-    Returns a dictionary with the id's of the gateways (XOR Split) as keys and the list of first target tasks as values
+    Returns a dictionary with the id's of the gateways (XOR Split) as keys and a nested dictionary with the outgoing flows and the list with its first target tasks as values
     """
     gateways = {}
     for node in bpmn_model.get_nodes():
         if isinstance(node, pm4py.BPMN.ExclusiveGateway):
             out_arcs = node.get_out_arcs()
+
             if len(out_arcs) > 1:
-                targets = []
+                gateways[node.get_id()] = {}
+
                 for flow in out_arcs:
-                    targets.extend(list(get_first_target_tasks_from_gateway(flow)))
-                gateways[node.id] = targets
+
+                    path_id = flow.get_id()
+                    tasks = list(get_first_target_tasks_from_gateway(flow))
+                    gateways[node.get_id()][path_id] = tasks
+
     return gateways
--- src/process_elements_mapper.py
+++ src/process_elements_mapper.py
@@ -17,17 +17,21 @@ def check_bpmn_for_new_tasks(event_log_tasks: list, bpmn_model_tasks: dict) -> d
 
 def find_new_xor_split_gateways(as_is_bpmn: pm4py.BPMN, to_be_bpmn: pm4py.BPMN) -> dict:
     """
-    Compares the as-is and new BPMN model and returns a dictionary with the id's of the new gateways (XOR Split) as keys and the list of first target tasks as values
+    Compares the as-is and new BPMN model and returns a dictionary with the id's of the new gateways (XOR Split) as keys and a nested dictionary with the outgoing flows and the list with its first target tasks as values
     """
     as_is = get_xor_split_gateway_target_tasks(as_is_bpmn)
     to_be = get_xor_split_gateway_target_tasks(to_be_bpmn)
 
-    target_tasks_as_is = {tuple(sorted(tasks)) for tasks in as_is.values()}
+    target_tasks_as_is = {
+        tuple(sorted([task for task_list in paths.values() for task in task_list])) 
+        for paths in as_is.values()
+    }
 
     new_gateways = {}
-    for g_id, tasks_to_be in to_be.items():
-
-        if tuple(sorted(tasks_to_be)) not in target_tasks_as_is:
-            new_gateways[g_id] = tasks_to_be
+    for g_id, paths_to_be in to_be.items():
+        all_tasks_to_be = [task for task_list in paths_to_be.values() for task in task_list]
+        
+        if tuple(sorted(all_tasks_to_be)) not in target_tasks_as_is:
+            new_gateways[g_id] = paths_to_be
 
     return new_gateways
