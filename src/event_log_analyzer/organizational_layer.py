d2a1832523ed346377a6532aca3480f9e175fd36
Luca-Paul Schirmer <lucapaulschirmer@gmail.com>
2026-05-11 22:09:06 +0200

    Added a method for getting the resources that work on specific tasks (based on the event log)

--- src/event_log_analyzer/organizational_layer.py
+++ src/event_log_analyzer/organizational_layer.py
@@ -6,12 +6,11 @@ import pandas as pd
 
 def calculate_degree_of_change(bpmn_model: pm4py.BPMN, event_log: pd.DataFrame) -> float:
     """
-    Calculates the degree of change of a redesigned BPMN model in respect to the as-is process via Conformance Checking
+    Calculates the behavioral degree of change of a redesigned BPMN model in respect to the as-is process via conformance checking
     """
     net, initial_marking, final_marking = pm4py.convert_to_petri_net(bpmn_model)
 
     aligned_traces = alignments.apply(event_log, net, initial_marking, final_marking)
-
     fitness_result = fitness_evaluator.evaluate(aligned_traces, variant=fitness_evaluator.Variants.ALIGNMENT_BASED)
 
     log_fitness = fitness_result["log_fitness"]
@@ -24,3 +23,9 @@ def get_process_variants(event_log: pd.DataFrame) -> list:
     Returns a list with all variants, the process runs through
     """
     return list(pm4py.get_variants(event_log).keys())
+
+def get_resources_per_task(event_log: pd.DataFrame) -> dict:
+    """
+    Returns a dictionary with all tasks and its associated resources in the event log
+    """
+    return event_log.groupby("concept:name")["org:resource"].unique().apply(list).to_dict()
